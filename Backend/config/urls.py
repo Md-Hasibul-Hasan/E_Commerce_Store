28db6fbbae5afcb78df0c98b741fa3615a942f41
Md Hasibul Hasan <hasib.hasan.th@gmail.com>
2026-04-04 13:34:53 +0600

    'fiximg'

--- Backend/config/urls.py
+++ Backend/config/urls.py
@@ -10,5 +10,5 @@ urlpatterns = [
     path('', include('base.urls')),
 ]
 
-# if settings.DEBUG:
-#     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
